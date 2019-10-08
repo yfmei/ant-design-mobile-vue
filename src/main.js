ca04ff1d369f64789e8edee0b6d29920afead182
梅云飞 <meiyf@asiainfo-sec.com>
2019-10-08 14:46:37 +0800

    add main stylesheet

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import store from "@/store"
 
 import { scroll } from "@/utils/scroll"
 
+import "@/style/index.scss"
+
 // 注册一个全局自定义指令 `v-scroll`
 Vue.directive("scroll", function (el, binding) {
   scroll(binding.value)
