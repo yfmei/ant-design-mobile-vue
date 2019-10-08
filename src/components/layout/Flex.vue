f1701b5c139ad8349b417ce65b2ce199080f08f4
梅云飞 <meiyf@asiainfo-sec.com>
2019-10-08 14:46:23 +0800

    add router components

---
+++ src/components/layout/Flex.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex-container box-200">
+    <div class="box-100"></div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Flex"
+  }
+</script>
+
+<style scoped>
+  .flex-container {
+    display: flex;
+    justify-content: center; /** 水平居中**/
+    align-items: center;     /** 垂直居中**/
+  }
+
+</style>
