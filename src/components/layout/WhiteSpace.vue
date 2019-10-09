d7cf38eb39e277dbe4e2be2e4a23951fe10b0364
梅云飞 <meiyf@asiainfo-sec.com>
2019-10-09 16:15:46 +0800

    add whitespace component

---
+++ src/components/layout/WhiteSpace.vue
@@ -0,0 +1,42 @@
+<template>
+  <div :class="getClass"></div>
+</template>
+
+<script>
+  export default {
+    name: "WhiteSpace",
+    props: {
+      size: {
+        type: String,
+        default: "md"
+      }
+    },
+    computed: {
+      getClass() {
+        return "whitespace-" + this.size
+      }
+    }
+  }
+</script>
+
+<style type="text/css" scoped>
+  .whitespace-xs {
+    height: 3px;
+  }
+
+  .whitespace-sm {
+    height: 6px;
+  }
+
+  .whitespace-md {
+    height: 9px;
+  }
+
+  .whitespace-lg {
+    height: 15px;
+  }
+
+  .whitespace-xl {
+    height: 21px;
+  }
+</style>
