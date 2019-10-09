467b7161a2a7afdec780877b6619de821ab7e90c
梅云飞 <meiyf@asiainfo-sec.com>
2019-10-09 17:25:07 +0800

    create demo

---
+++ src/components/demo/layout/WhiteSpaceDemo.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <div class="block">Block</div>
+    <WhiteSpace size="xs"/>
+    <div class="block">Block</div>
+    <WhiteSpace size="sm"/>
+    <div class="block">Block</div>
+    <WhiteSpace size="md"/>
+    <div class="block">Block</div>
+    <WhiteSpace size="lg"/>
+    <div class="block">Block</div>
+    <WhiteSpace size="xl"/>
+    <div class="block">Block</div>
+  </div>
+</template>
+
+<script>
+  import WhiteSpace from "@/components/layout/WhiteSpace"
+  export default {
+    name: "WhiteSpaceDemo",
+    components: { WhiteSpace }
+  }
+</script>
+
+<style scoped>
+  .block {
+    text-align: center;
+    background-color: #ebebef;
+    color: #bbb;
+    height: 30px;
+    line-height: 30px;
+    width: 100%;
+  }
+</style>
---
+++ src/components/demo/layout/WingBlankDemo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <WingBlank size="lg"><div class="block">Block</div></WingBlank>
+
+    <WhiteSpace size="lg"/>
+    <WingBlank size="md"><div class="block">Block</div></WingBlank>
+
+    <WhiteSpace size="lg"/>
+    <WingBlank size="sm"><div class="block">Block</div></WingBlank>
+  </div>
+</template>
+
+<script>
+  import WingBlank from "@/components/layout/WingBlank"
+  import WhiteSpace from "@/components/layout/WhiteSpace"
+  export default {
+    name: "WingBlankDemo",
+    components: { WingBlank, WhiteSpace }
+  }
+</script>
+
+<style scoped>
+  .block {
+    text-align: center;
+    background-color: #ebebef;
+    color: #bbb;
+    height: 30px;
+    line-height: 30px;
+    width: 100%;
+  }
+</style>
