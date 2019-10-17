cfd746e2019dfb5c0e38d8a98fc2425142c1d7aa
梅云飞 <meiyf@asiainfo-sec.com>
2019-10-17 11:29:11 +0800

    uniform classNames

--- src/components/layout/Flex.vue
+++ src/components/layout/Flex.vue
@@ -1,10 +1,12 @@
 <template>
-  <div :style="flexStyle">
+  <div :class="getClass">
     <slot></slot>
   </div>
 </template>
 
 <script>
+  import { classNames } from "@/components/utils/classNames"
+
   export default {
     name: "Flex",
     props: {
@@ -14,21 +16,18 @@
       // column
       // column-reverse
       direction: {
-        type: String,
-        default: "row"
+        type: String
       },
       // 子容器宽度超过父容器时如何换行
       // nowrap: 不换行
       // wrap: 换行
       // wrap-reverse： 从下到上换行
       wrap: {
-        type: String,
-        default: "nowrap"
+        type: String
       },
       // direction 和 wrap 的缩写 "row nowrap", data() 中的顺序决定优先级
       flow: {
-        type: String,
-        default: "row nowrap"
+        type: String
       },
       // 设置子容器沿主轴排列
       // center: 居中
@@ -37,8 +36,7 @@
       // space-around: 每个子容器之间的间隔相同, 因此项目之间的间距比子容器与父容器边框的间距大一倍
       // space-between: 首尾两个子容器紧贴父容器, 子容器间隔相同
       justify: {
-        type: String,
-        default: "start"
+        type: String
       },
       // 设置子容器沿交叉轴排列
       // center: 居中
@@ -48,45 +46,122 @@
       // stretch: 拉伸占满整个容器
       align: {
         type: String,
-        default: "start"
+        default: "center"
+      },
+      alignContent: {
+        type: String
       }
     },
     data() {
-      let that = this
-      if (!that.$options.propsData.flow) {
-        // flow 不存在就用 direction + wrap 组合
-        that.$flow = that.direction + " " + that.wrap
-      }
       return {
-        flexStyle: {
-          display: "flex",
-          // flexDirection: that.direction,
-          // flexWrap: that.wrap,
-          flexFlow: that.$flow,
-          justifyContent: that.getJustifyProp(that.justify),
-          alignItems: that.getAlignProp(that.align)
-        },
-        $flow: that.flow
+        prefixCls: "am-flexbox"
+      }
+    },
+    computed: {
+      getClass() {
+        let that = this
+        return classNames(that.prefixCls, that.className,
+                          {
+                            [that.prefixCls + "-dir-" + that.direction]: that.direction,
+                            [that.prefixCls + "-" + that.wrap]: that.wrap,
+                            [that.prefixCls + "-justify-" + that.justify]: that.justify,
+                            [that.prefixCls + "-align-" + that.align]: that.align,
+                            [that.prefixCls + "-align-content-" + that.alignContent]: that.alignContent
+                          }
+        )
       }
     },
     methods: {
-      getJustifyProp(prop) {
-        if (prop === "center") return prop
-        return ["around", "between"].includes(prop) ? "space-" + prop : "flex-" + prop
-      },
-      getAlignProp(prop) {
-        // todo stretch not working
-        return ["center", "baseline", "stretch"].includes(prop) ? prop : "flex-" + prop
-      }
+
     }
   }
 </script>
 
-<style scoped>
-  .flex-container {
+<style lang="scss">
+
+  $flexPrefixClz: am-flexbox;
+
+  .#{$flexPrefixClz}{
     display: flex;
-    justify-content: center; /** 水平居中**/
-    align-items: center;     /** 垂直居中**/
+
+    &-dir-row {
+      flex-direction: row;
+    }
+    &-dir-row-reverse {
+      flex-direction: row-reverse;
+    }
+    &-dir-column {
+      flex-direction: column;
+    }
+    &-dir-column-reverse {
+      flex-direction: column-reverse;
+    }
+
+    &-nowrap {
+      flex-wrap: nowrap;
+    }
+    &-wrap {
+      flex-wrap: wrap;
+    }
+    &-wrap-reverse {
+      flex-wrap: wrap-reverse;
+    }
+
+    &-justify-start{
+      justify-content: start;
+    }
+    &-justify-end{
+      justify-content: end;
+    }
+    &-justify-center{
+      justify-content: center;
+    }
+    &-justify-between{
+      justify-content: space-between;
+    }
+    &-justify-around{
+      justify-content: space-around;
+    }
+
+    &-align-start{
+      align-items: flex-start;
+    }
+    &-align-end{
+      align-items: flex-end;
+    }
+    &-align-center{
+      align-items: center;
+    }
+    &-align-baseline{
+      align-items: baseline;
+    }
+    &-align-stretch{
+      align-items: stretch;
+    }
+
+    &-align-content-start {
+      align-content: flex-start;
+    }
+
+    &-align-content-end {
+      align-content: flex-end;
+    }
+
+    &-align-content-center {
+      align-content: center;
+    }
+
+    &-align-content-between {
+      align-content: space-between;
+    }
+
+    &-align-content-around {
+      align-content: space-around;
+    }
+
+    &-align-content-stretch {
+      align-content: stretch;
+    }
   }
 
 </style>
